64d980ecf1e2d22efe35ac2e28d7b26620477a18
Z-BOT999 <91157924+Z-BOT999@users.noreply.github.com>
2021-09-26 18:31:17 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -122,7 +122,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '94771323434@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./Amalser/alive.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['alive','z','ammo','Asitha','duka','fuck','Gm','help','himashi','Hm','imaah','nawangi','love','nidusha','ooi','pakaya','palayan','praneeth','song','tharahai']
+const array = ['alive','z','ammo','Asitha','duka','fuck','Gm','help','himashi','Hm','imaah','nawangi','love','nidusha','ooi','pakaya','palayan','praneeth','tharahai']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
